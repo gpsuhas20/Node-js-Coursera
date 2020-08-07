0b709fe39ef29b9016b0948d2385e47bb3a14b4c
gpsuhas20 <gpsuhas20@gmail.com>
2020-08-07 14:59:19 +0530

    http static files

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 const http=require('http')
-
+const fs=require('fs')// used to access the files 
+const path=require('path')// used to get the path of the files.
 const hostname="localhost"
 const port=3000;
 // we are creating a server and  setting the values to the response
 const server=http.createServer((req,res)=>
 {
-    console.log(req.headers);
-    res.statusCode=200;
-    res.setHeader('Content-type','text/html');
-    res.end('<html><body><h1>hello world</h1></body></html>')
+    console.log("Request for "+req.url+ 'by method'+req.method);
+  if(req.method=="GET")
+  {
+      var fileUrl;
+      if(req.url=='/') fileUrl='/index.html'
+      else fileUrl=req.url
+
+      var filePath=path.resolve('./public'+fileUrl)// to the complete path
+      const fileExt=path.extname(filePath)
+      if(fileExt=='.html')
+      // fs.exists checks whether file exists in the given filepath if it is there then it return a  exists(true /false) which is passed an arguement to the callback function.
+      //instead we can do if (fs.exists){function} instead using the callback function.
+        fs.exists(filePath,(exists)=>
+        {
+                if(!exists)
+                {
+                    res.statusCode=404
+                    res.setHeader('Content-Type','text/html')
+                    res.end('<html><body><h1>Error 404:'+fileUrl+'not found</h2></body></html')
+                    return
+                }
+        
+        res.statusCode=200
+        res.setHeader('Content-Type','text/html')
+        fs.createReadStream(filePath).pipe(res)
+            })
+            else{
+                res.statusCode=404
+                res.setHeader('Content-Type','text/html')
+                res.end('<html><body><h1>Error 404:'+req.method+'not supported</h2></body></html')
+                return
+
+            }
+  }
 }
 )
 server.listen(port,hostname,()=>
